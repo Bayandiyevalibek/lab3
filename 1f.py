7e2cf9f563645a7399365587b88a4638be9df5a2
Bayandiyevalibek <a_bayandiyev@kbtu.kz>
2025-02-05 02:25:15 +0500

    Add files via upload

---
+++ 1f.py
@@ -0,0 +1,5 @@
+def gram_to_ounces(gram):
+    return gram / 28.3495231
+
+gram = float(input("Введите грам: "))
+print(gram_to_ounces(gram))
---
+++ 2f.py
@@ -0,0 +1,5 @@
+def celci(f):
+    return (f-32)* 5/9
+
+f = float(input())
+print(celci(f))
